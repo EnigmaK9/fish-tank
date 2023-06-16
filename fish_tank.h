fbe5e0f78f249d9ce3d51364d287e11e6c221b51
Ignacio Padilla <enigmak9@protonmail.com>
2023-06-15 21:50:13 -0500

    added comments to the code

--- fish_tank.h
+++ fish_tank.h
@@ -1,8 +1,10 @@
+// This header file is used to declare the function prototypes for the fish tank related functions
+
 #ifndef FISH_TANK_H
 #define FISH_TANK_H
 
-void displayFishTank();
-void specialKeys(int key, int x, int y);
-void keyboard(unsigned char key, int x, int y);
+void displayFishTank(); // Function prototype for displaying the fish tank
+void specialKeys(int key, int x, int y); // Function prototype for handling special keys
+void keyboard(unsigned char key, int x, int y); // Function prototype for handling keyboard input
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
+// 🌟✨ Programa creado por Carlos Ignacio Padilla Herrera ✨🌟
+
 #include <GL/glut.h>
 #include "fish_tank.h"
 
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(500, 500);
-    glutCreateWindow("OpenGL Fish Tank");
+    glutInit(&argc, argv); // Inicializa GLUT
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); // Configura el modo de visualización
+    glutInitWindowSize(500, 500); // Establece el tamaño de la ventana
+    glutCreateWindow("OpenGL Fish Tank"); // Crea una ventana con el título especificado
 
-    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST); // Habilita el test de profundidad para la representación en 3D
 
-    glutDisplayFunc(displayFishTank);
-    glutSpecialFunc(specialKeys);
-    glutKeyboardFunc(keyboard);
-    glutMainLoop();
+    glutDisplayFunc(displayFishTank); // Registra la función de visualización del tanque de peces
+    glutSpecialFunc(specialKeys); // Registra la función para manejar las teclas especiales
+    glutKeyboardFunc(keyboard); // Registra la función para manejar las teclas normales del teclado
+    glutMainLoop(); // Inicia el bucle principal de GLUT
 
     return 0;
 }
