c1ec72e2e914607daa1da11c1c6a6e6f4ed130ab
Ignacio Padilla <enigmak9@protonmail.com>
2023-06-15 22:10:49 -0500

    fish tank modified

--- fish_tank.cpp
+++ fish_tank.cpp
@@ -22,40 +22,69 @@ void displayFishTank()
     float depth = 0.75f;
 
     // Renderizar la pecera
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     glBegin(GL_QUADS);
-    glColor3f(0.0f, 0.0f, 1.0f);  // Color azul
+    glColor4f(0.0f, 0.0f, 1.0f, 0.3f);  // Color azul con transparencia
+
     // Cara frontal
     glVertex3f(-width / 2, -height / 2, depth / 2);
     glVertex3f(width / 2, -height / 2, depth / 2);
     glVertex3f(width / 2, height / 2, depth / 2);
     glVertex3f(-width / 2, height / 2, depth / 2);
+
     // Cara trasera
     glVertex3f(-width / 2, -height / 2, -depth / 2);
     glVertex3f(width / 2, -height / 2, -depth / 2);
     glVertex3f(width / 2, height / 2, -depth / 2);
     glVertex3f(-width / 2, height / 2, -depth / 2);
-    // Caras laterales
+
+    // Caras laterales (transparentes)
+    glColor4f(0.0f, 0.0f, 1.0f, 0.1f);  // Color azul más transparente
+
+    // Lado izquierdo
     glVertex3f(-width / 2, -height / 2, depth / 2);
     glVertex3f(-width / 2, -height / 2, -depth / 2);
     glVertex3f(-width / 2, height / 2, -depth / 2);
     glVertex3f(-width / 2, height / 2, depth / 2);
+
+    // Lado derecho
     glVertex3f(width / 2, -height / 2, depth / 2);
     glVertex3f(width / 2, -height / 2, -depth / 2);
     glVertex3f(width / 2, height / 2, -depth / 2);
     glVertex3f(width / 2, height / 2, depth / 2);
-    // Caras superior e inferior
-    glVertex3f(-width / 2, height / 2, depth / 2);
-    glVertex3f(-width / 2, height / 2, -depth / 2);
-    glVertex3f(width / 2, height / 2, -depth / 2);
-    glVertex3f(width / 2, height / 2, depth / 2);
+
+    // Cara inferior
     glVertex3f(-width / 2, -height / 2, depth / 2);
     glVertex3f(-width / 2, -height / 2, -depth / 2);
     glVertex3f(width / 2, -height / 2, -depth / 2);
     glVertex3f(width / 2, -height / 2, depth / 2);
+
+    // Cara superior
+    //glVertex3f(-width / 2, height / 2, depth / 2);
+    //glVertex3f(-width / 2, height / 2, -depth / 2);
+    //glVertex3f(width / 2, height / 2, -depth / 2);
+    //glVertex3f(width / 2, height / 2, depth / 2);
+
     glEnd();
+
+    glDisable(GL_BLEND);
+
+    // Renderizar el pez (solo como ejemplo)
+    glPushMatrix();
+    glColor3f(1.0f, 1.0f, 0.0f);  // Color amarillo para el pez
+    glTranslatef(0.0f, 0.0f, 0.0f);  // Posición del pez en el centro de la pecera
+    glutSolidSphere(0.1f, 20, 20);  // Representación del pez como una esfera
+    glPopMatrix();
 
     glutSwapBuffers();
 }
+
+// Variables para el movimiento de la cámara
+float cameraX = 0.0f;
+float cameraY = 0.0f;
+float cameraZ = 5.0f;
 
 void specialKeys(int key, int x, int y)
 {
@@ -81,7 +110,7 @@ void specialKeys(int key, int x, int y)
 
 void keyboard(unsigned char key, int x, int y)
 {
-    // Handle rotation based on WASD keys
+    // Handle rotation and movement based on WASD keys
     switch (key) {
         case 'w':
             angleX -= 5.0f;
@@ -95,8 +124,21 @@ void keyboard(unsigned char key, int x, int y)
         case 'd':
             angleY += 5.0f;
             break;
+        case 'q':
+            cameraZ -= 0.1f;
+            break;
+        case 'e':
+            cameraZ += 0.1f;
+            break;
+        case 'r':
+            cameraY += 0.1f;
+            break;
+        case 'f':
+            cameraY -= 0.1f;
+            break;
     }
 
     // Redraw the scene
     glutPostRedisplay();
 }
+
