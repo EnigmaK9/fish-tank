de4303163f18c07494bf71031c5e91238291ef0f
Ignacio Padilla <enigmak9@protonmail.com>
2023-06-18 16:23:32 -0500

    cleaned code in camera.h

--- include/camera.h
+++ include/camera.h
@@ -1,39 +1,37 @@
 #include "main.h"
 
-#define CAMERASPEED 0.07f // The Camera Speed
+#define CAMERASPEED 0.07f // Camera Speed
 
-//////////////////////////////////////
-// The tVector3 Struct
-//////////////////////////////////////
-typedef struct tVector3 // Expanded 3D vector struct
+// 3D vector struct
+typedef struct tVector3
 {			
     tVector3() {} // Constructor
-    tVector3(float new_x, float new_y, float new_z) // Initialize constructor	 
+    tVector3(float new_x, float new_y, float new_z)
     {
         x = new_x;
         y = new_y;
         z = new_z;
     }
 
-    // Overload + operator to facilitate vector addition
+    // Operator overloading for vector addition
     tVector3 operator+(tVector3 vVector)
     {
         return tVector3(vVector.x + x, vVector.y + y, vVector.z + z);
     }
 
-    // Overload - operator to facilitate vector subtraction
+    // Operator overloading for vector subtraction
     tVector3 operator-(tVector3 vVector)
     {
         return tVector3(x - vVector.x, y - vVector.y, z - vVector.z);
     }
 
-    // Overload * operator to facilitate multiplication by scalars
+    // Operator overloading for scalar multiplication
     tVector3 operator*(float number)
     {
         return tVector3(x * number, y * number, z * number);
     }
 
-    // Overload / operator to facilitate division by a scalar
+    // Operator overloading for scalar division
     tVector3 operator/(float number)
     {
         return tVector3(x / number, y / number, z / number);
@@ -42,9 +40,7 @@ typedef struct tVector3 // Expanded 3D vector struct
     float x, y, z; // 3D vector coordinates
 } tVector3;
 
-//////////////////////////////////////
-// The CCamera Class
-//////////////////////////////////////
+// Camera class
 class CCamera 
 {
 public:
@@ -52,10 +48,19 @@ public:
     tVector3 mView;		
     tVector3 mUp;			
 
-    void Strafe_Camera(float speed); // Move the camera sideways
-    void Move_Camera(float speed); // Move the camera forward/backward
-    void Rotate_View(float speed); // Rotate the camera view
-    void UpDown_Camera(float speed); // Move the camera up/down
+    // Move the camera sideways
+    void Strafe_Camera(float speed);
+
+    // Move the camera forward/backward
+    void Move_Camera(float speed);
+
+    // Rotate the camera view
+    void Rotate_View(float speed);
+
+    // Move the camera up/down
+    void UpDown_Camera(float speed);
+
+    // Set the position and view of the camera
     void Position_Camera(float pos_x, float pos_y, float pos_z,
                          float view_x, float view_y, float view_z,
                          float up_x, float up_y, float up_z);
